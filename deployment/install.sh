e4bda7b76adcf7e841626354d6c7bbff492f6ae9
Ryder Damen <dev@ryderdamen.com>
2021-05-13 15:31:55 -0400

    updating install script

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # install_raspberry_pi.sh
 
+cd ../
+
 echo "Installing base dependencies"
 sudo apt-get update
 sudo apt-get install -y ffmpeg \
     mpg321 \
-    espeak
+    espeak \
+    python3-pip
 
-cd ../
-
-python -m pip install virtualenv
-
-python -m virtualenv -p python3 env
+python3 -m pip install virtualenv
+python3 -m virtualenv -p python3 env
 . env/bin/activate
+
 pip install -r src/requirements.txt
